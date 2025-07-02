559a2d037d8485f5fd0b048aeae09bc904976884
Elishama Vayanza <vayanzaelishama@gmail.com>
2025-07-02 23:05:04 +0200

    Admin: ameliioration de l'affichage de notification

--- src/main/java/com/association/view/styles/Colors.java
+++ src/main/java/com/association/view/styles/Colors.java
@@ -41,8 +41,7 @@ public class Colors {
 
     // Dans la classe Colors
     public static final Color UNREAD_NOTIFICATION = new Color(0, 100, 200); // Bleu pour les non-lues
-    public static Color CURRENT_UNREAD_NOTIFICATION = UNREAD_NOTIFICATION; // Par défaut
-    // Dans Colors.java
+    public static Color CURRENT_UNREAD_NOTIFICATION = new Color(0, 100, 200); // Bleu par défaut
     public static final Color SELECTION_BACKGROUND = new Color(0, 123, 255, 50);
     // Utility method to toggle between themes
     public static void setDarkTheme(boolean enabled) {
